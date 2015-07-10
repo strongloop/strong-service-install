9a52bb46b471cd7baf85cdf88f18687b504ed0dd
Ryan Graham <ryan@strongloop.com>
2015-07-09 18:49:50 -0700

    test: cleanup vagrant tests

--- test/test-end-to-end.sh
+++ test/test-end-to-end.sh
@@ -1,50 +1,45 @@
-#!/bin/sh
+#!/bin/bash
+cd $(dirname "${BASH_SOURCE[0]}")
+source common.sh
 
 if ! which vagrant; then
-  echo "ok # skip vagrant tests, vagrant not found"
-  echo "no vagrant" >&2
+  skip "vagrant not found"
   exit
 fi
 
 if test -n "$SKIP_VAGRANT"; then
-  echo "ok # skip vagrant tests, disabled with SKIP_VAGRANT"
-  echo "skip vagrant" >&2
+  skip "disabled with SKIP_VAGRANT"
   exit
 fi
 
-NODE_VERSION=0.10.33
-export NODE_TGZ="node-v$NODE_VERSION-linux-x64.tar.gz"
+NODE_VERSION=0.10.40
+NODE_TGZ="node-v$NODE_VERSION-linux-x64.tar.gz"
 
 if ! test -f $NODE_TGZ; then
   curl http://nodejs.org/dist/v$NODE_VERSION/$NODE_TGZ > $NODE_TGZ
 fi
 
-export PKG=$(npm pack ..)
+PKG=$(npm pack ..)
 
-vagrant destroy --force ubuntu centos6 centos7 >&2
-vagrant up --provision --parallel ubuntu centos6 centos7 >&2
+comment "destroying any existing VMs..."
+PKG_NAME=$PKG NODE_TGZ=$NODE_TGZ vagrant destroy --force
+# vagrant destroy --force ubuntu centos6 centos7
 
-FAIL=0
+comment "creating VMs"
+PKG_NAME=$PKG NODE_TGZ=$NODE_TGZ vagrant up --parallel \
+  && ok "created vagrant VMs" \
+  || fail "failed to provision VMs"
 
-if curl -sI 'http://127.0.0.1:10001/foo/bar?baz=quux' >&2; then
-  echo 'ok # Upstart 1.4 on Ubuntu 12.04'
-else
-  echo 'not ok # Upstart 1.4 on Ubuntu 12.04 failed'
-  export FAIL=1
-fi
+curl -sI 'http://127.0.0.1:10001/foo/bar?baz=quux' >&2 \
+  && ok 'Upstart 1.4 on Ubuntu 14.04' \
+  || fail 'Upstart 1.4 on Ubuntu 14.04'
 
-if curl -sI 'http://127.0.0.1:10002/foo/bar?baz=quux' >&2; then
-  echo 'ok # Upstart 0.6 on CentOS 6.5'
-else
-  echo 'not ok # Upstart 0.6 on CentOS 6 failed'
-  export FAIL=1
-fi
+curl -sI 'http://127.0.0.1:10002/foo/bar?baz=quux' >&2 \
+  && ok 'Upstart 0.6 on CentOS 6.5' \
+  || fail 'Upstart 0.6 on CentOS 6.5'
 
-if curl -sI 'http://127.0.0.1:10003/foo/bar?baz=quux' >&2; then
-  echo 'ok # systemd on CentOS 7'
-else
-  echo 'not ok # systemd on CentOS 7 failed'
-  export FAIL=1
-fi
+curl -sI 'http://127.0.0.1:10003/foo/bar?baz=quux' >&2 \
+  && ok 'systemd on CentOS 7' \
+  || fail 'systemd on CentOS 7'
 
-exit $FAIL
+assert_report
