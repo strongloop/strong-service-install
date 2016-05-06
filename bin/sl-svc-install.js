f8d7f886b8e2606b2c967388ce6f4a9392f2615b
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 11:52:37 -0700

    update copyright notices and license

--- bin/sl-svc-install.js
+++ bin/sl-svc-install.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: strong-service-install
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
 
 var install = require('../');
 var minimist = require('minimist');
